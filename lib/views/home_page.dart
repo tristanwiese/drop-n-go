9400de79397f3068cebbd61e6cc71d25c2a7f197
tristanwiese <tristanwiese7472@gmail.com>
2023-04-13 17:01:37 +0200

    Refactor

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -14,13 +14,13 @@ import '../services/utils.dart';
 // ignore: must_be_immutable
 class MyHomePage extends StatefulWidget {
   MyHomePage({
-    super.key, 
-    required this.title, 
+    super.key,
+    required this.title,
     required this.currentPosition,
     required this.weather,
     required this.temp,
     required this.favorites,
-    });
+  });
   QuerySnapshot<Map<String, dynamic>>? favorites;
   final String title;
   Position? currentPosition;
@@ -32,72 +32,90 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: (){navReplace(context, const Initializer());},
-        child: const Icon(Icons.gps_fixed_outlined)),
+          onPressed: () {
+            navReplace(context, const Initializer());
+          },
+          child: const Icon(Icons.gps_fixed_outlined)),
       body: Center(
         child: Row(
-          
           children: [
             MyDrawer(favorites: widget.favorites),
-            Expanded(
-              child: SizedBox(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Logo(),
-                        Icon(
-                          Icons.location_on,
-                          size: 60,
-                          color: Colors.green,
-                        )
-                      ],
-                    ),
-                    CircleAvatar(
-                      radius: 40,
-                      backgroundColor: Colors.green[100],
-                      child: IconButton(
-                        // ignore: avoid_returning_null_for_void
-                        onPressed: (){
-                          navPush(context, MapWidget(lat: widget.currentPosition!.latitude, lon: widget.currentPosition!.longitude));
-                          NearbyPlaces(lon: widget.currentPosition!.longitude, lat: widget.currentPosition!.latitude, radius: 1000).get();
-                        },
-                        icon: const Icon(Icons.location_on_outlined),
-                        iconSize: 60,
-                        color: Colors.green,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: SizedBox(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            TextCard(text: 'LAT: ${widget.currentPosition!.latitude}'),
-                            const SizedBox(width: 20),
-                            TextCard(text:'LON: ${widget.currentPosition!.longitude}'),
-                            const SizedBox(width: 20),
-                            TextCard(text: 'TEMP C: ${widget.temp}'),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Column(),
+            centrColumn(context),
+            const RightGap(),
           ],
         ),
       ),
     );
+  }
+
+
+
+
+
+
+  Expanded centrColumn(BuildContext context) {
+    return Expanded(
+      child: SizedBox(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Logo(),
+            CircleAvatar(
+              radius: 40,
+              backgroundColor: Colors.green[100],
+              child: IconButton(
+                // ignore: avoid_returning_null_for_void
+                onPressed: () {
+                  navPush(
+                      context,
+                      MapWidget(
+                          lat: widget.currentPosition!.latitude,
+                          lon: widget.currentPosition!.longitude));
+                  NearbyPlaces(
+                          lon: widget.currentPosition!.longitude,
+                          lat: widget.currentPosition!.latitude,
+                          radius: 1000)
+                      .get();
+                },
+                icon: const Icon(Icons.location_on_outlined),
+                iconSize: 60,
+                color: Colors.green,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: SizedBox(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextCard(text: 'LAT: ${widget.currentPosition!.latitude}'),
+                    const SizedBox(width: 20),
+                    TextCard(text: 'LON: ${widget.currentPosition!.longitude}'),
+                    const SizedBox(width: 20),
+                    TextCard(text: 'TEMP C: ${widget.temp}'),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RightGap extends StatelessWidget {
+  const RightGap({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
   }
 }
 
@@ -111,8 +129,9 @@ class TextCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(text, style: const TextStyle(fontWeight: FontWeight.bold),);
+    return Text(
+      text,
+      style: const TextStyle(fontWeight: FontWeight.bold),
+    );
   }
 }
-
-
--- lib/views/my_drawer.dart
+++ lib/views/my_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:drop_n_go/Views/initializer.dart';
+import 'package:drop_n_go/main.dart';
 import 'package:drop_n_go/services/nav.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -21,53 +22,66 @@ class _MyDrawerState extends State<MyDrawer> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.zero,
       width: 70,
       margin: EdgeInsets.zero,
       decoration: const BoxDecoration(color: Colors.green),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          IconButton(
-            icon: const Icon(Icons.star_border),
-            onPressed: () => showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: const Text('Favorites'),
-                  content: favoritePage(context),
-                );
-              },
-            ),
-          ),
+          favoriteButton(),
+          const SizedBox(),
+          const SignOut(),
         ],
       ),
     );
   }
 
+
+
+
+
+  IconButton favoriteButton() {
+    return IconButton(
+          icon: const Icon(Icons.star_border),
+          iconSize: 40,
+          color: Colors.white,
+          onPressed: () => showFavoritepage()
+        );
+  }
+
   Widget favoritePage(BuildContext context) {
-    return SizedBox(
-      width: 400,
-      height: 400,
-      child: ListView.builder(
-        itemCount: widget.favorites!.docs.length,
-        itemBuilder: (BuildContext context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: ListTile(
-              onTap: () => navPush(context, MapWidget(lat: widget.favorites!.docs[index]['lat'], lon:widget.favorites!.docs[index]['lon'])),
-              title: Text(widget.favorites!.docs[index]['id']),
-              trailing: IconButton(
-                  onPressed: () => deleteFav(widget.favorites!.docs[index]['id']),
-                  icon: const Icon(
-                    Icons.delete,
-                    color: Colors.red,
-                  )),
-              hoverColor: Colors.lightGreen[300],
-              shape: const StadiumBorder(side: BorderSide(width: 1)),
-            ),
-          );
-        },
-        
+    return AlertDialog(
+      title: const Text('Favorites'),
+      content: SizedBox(
+        width: 400,
+        height: 400,
+        child: ListView.builder(
+          itemCount: widget.favorites!.docs.length,
+          itemBuilder: (BuildContext context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: ListTile(
+                onTap: () => navPush(
+                    context,
+                    MapWidget(
+                        lat: widget.favorites!.docs[index]['lat'],
+                        lon: widget.favorites!.docs[index]['lon'])),
+                title: Text(widget.favorites!.docs[index]['id']),
+                trailing: IconButton(
+                    onPressed: () =>
+                        deleteFav(widget.favorites!.docs[index]['id']),
+                    icon: const Icon(
+                      Icons.delete,
+                      color: Colors.red,
+                    )),
+                hoverColor: Colors.lightGreen[300],
+                shape: const StadiumBorder(side: BorderSide(width: 1)),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
@@ -88,5 +102,33 @@ class _MyDrawerState extends State<MyDrawer> {
 
     // ignore: use_build_context_synchronously
     navReplace(context, const Initializer());
+  }
+
+  showFavoritepage(){
+    return showDialog(
+              context: context,
+      builder: (BuildContext context) {
+                return favoritePage(context);
+              },
+            );
+  }
+}
+
+class SignOut extends StatelessWidget {
+  const SignOut({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        FirebaseAuth.instance.signOut();
+        navReplace(context, const MainPage());
+      },
+      icon: const Icon(Icons.logout_rounded),
+      color: Colors.red[300],
+      iconSize: 40,
+    );
   }
 }
