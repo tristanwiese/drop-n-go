70a0edf88373de210766812ffd5c208b5a412d31
tristanwiese <tristanwiese7472@gmail.com>
2023-04-19 09:58:39 +0200

    Request Headers added

--- lib/services/nearby_places.dart
+++ lib/services/nearby_places.dart
@@ -4,10 +4,6 @@ import 'package:drop_n_go/models/nearby_locations_data.dart';
 import 'package:http/http.dart' as http;
 
 class NearbyPlaces {
-  final corsHeaders = {
-    'Access-Control-Allow-Origin': '*',
-  };
-
   int radius;
   double lat;
   double lon;
@@ -15,28 +11,35 @@ class NearbyPlaces {
   NearbyPlaces({required this.lat, required this.lon, required this.radius});
 
   Future<NearbyLocationsData?> get() async {
-    String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?&location=$lat,$lon&radius=$radius&key=AIzaSyASHyqPfVoEeH4KDaCKbz4Vr6ZM1vzdSO4";
-    
-      var response = await http.get(Uri.parse(url), headers: corsHeaders);
-      if (response.statusCode == 200) {
-        String json = response.body;
-        print("Radius = $url");
-        return nearbyLocationsDataFromJson(json);
-      } else {
-        return null;
-      }
+    final corsHeaders = {
+      'Access-Control-Allow-Origin': '*',
+      "Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, DELETE, HEAD",
+    };
+
+    String url =
+        "https://maps.googleapis.com/maps/api/place/nearbysearch/json?&location=$lat,$lon&radius=$radius&key=AIzaSyASHyqPfVoEeH4KDaCKbz4Vr6ZM1vzdSO4";
+
+    var response = await http.get(Uri.parse(url), headers: corsHeaders);
+    if (response.statusCode == 200) {
+      String json = response.body;
+      print("Radius = $url");
+      return nearbyLocationsDataFromJson(json);
+    } else {
+      return null;
+    }
   }
+
   Future<NearbyLocationsData?> getMore(token) async {
     String url =
         "https://maps.googleapis.com/maps/api/place/nearbysearch/json?&pagetoken=$token&key=AIzaSyASHyqPfVoEeH4KDaCKbz4Vr6ZM1vzdSO4";
-    
-      var response = await http.get(Uri.parse(url));
-      if (response.statusCode == 200) {
-        String json = response.body;
-        print("Radius = $url");
-        return nearbyLocationsDataFromJson(json);
-      } else {
-        return null;
-      }
+
+    var response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      String json = response.body;
+      print("Radius = $url");
+      return nearbyLocationsDataFromJson(json);
+    } else {
+      return null;
+    }
   }
 }
