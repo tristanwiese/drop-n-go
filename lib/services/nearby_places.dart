1f4ba26504575ef6c39543b3522f45a4ccc8f631
tristanwiese <tristanwiese7472@gmail.com>
2024-08-21 11:52:28 +0200

    na
    na

--- lib/services/nearby_places.dart
+++ lib/services/nearby_places.dart
@@ -1,5 +1,9 @@
 // ignore_for_file: avoid_print
 
+// import 'dart:math';
+
+import 'dart:developer';
+
 import 'package:drop_n_go/models/nearby_locations_data.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,24 +16,28 @@ class NearbyPlaces {
   NearbyPlaces({required this.lat, required this.lon, required this.radius});
 
   Future<NearbyLocationsData?> get() async {
+    // String url =
+    //     "https://etrainer.co.za/tristan/google_nearby_places_api/get.places.php";
     String url =
-        "https://etrainer.co.za/tristan/google_nearby_places_api/get.places.php";
-    var response = await http.post(Uri.parse(url), body: {
-      'get': 'normal',
-      'lat': '$lat',
-      'lon': '$lon',
-      'rad': '$radius',
-      'key': _key,
-    });
-    if (response.statusCode == 200) {
-      String json = response.body;
+        "https://etrainer.co.za/tristan/php/whatsapp-api/receive_api.php";
+    var res = await http.post(Uri.parse(url), body: "test");
+    log(res.body);
+    // var response = await http.post(Uri.parse(url), body: {
+    //   'get': 'normal',
+    //   'lat': '$lat',
+    //   'lon': '$lon',
+    //   'rad': '$radius',
+    //   'key': _key,
+    // });
+    // if (response.statusCode == 200) {
+    //   String json = response.body;
 
-      print(response.body);
+    //   print(response.body);
 
-      return nearbyLocationsDataFromJson(json);
-    } else {
-      return null;
-    }
+    //   return nearbyLocationsDataFromJson(json);
+    // } else {
+    //   return null;
+    // }
   }
 
   Future<NearbyLocationsData?> getMore(token) async {
